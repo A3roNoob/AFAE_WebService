85eea6933647deea9b2362852952027399cbb157
EmeraldWhovian <boutryguillaume1@gmail.com>
2017-06-22 13:19:43 +0200

    Correctif Note Vendeur
    Oublie d'ajouter le mois et l'année de la foire.

--- resources/impressions/EtiquetteVendeur.php
+++ resources/impressions/EtiquetteVendeur.php
@@ -57,6 +57,37 @@ session_start();
 <body>
 <?php
 
+function monthToFrench($m){
+    switch($m){
+        case 1:
+            return "janvier";
+        case 2:
+            return "f&eacute;vrier";
+        case 3:
+            return "mars";
+        case 4:
+            return "avril";
+        case 5:
+            return "mai";
+        case 6:
+            return "juin";
+        case 7:
+            return "juillet";
+        case 8:
+            return "aout";
+        case 9:
+            return "septembre";
+        case 10:
+            return "octobre";
+        case 11:
+            return "novembre";
+        case 12:
+            return "d&eacute;cembre";
+        default:
+            return "n'existe pas";
+    }
+}
+
 function printUser($userArr){
     $cpt = 0;
     foreach ($userArr as $user) {
@@ -67,7 +98,7 @@ function printUser($userArr){
             $class = "two";
         ?>
         <div class="<?php echo $class ?>">
-            <h4><?php echo $_SESSION['foire']->nomFoire(); ?></h4>
+            <h4><?php echo $_SESSION['foire']->nomFoire(). " - " . monthToFrench(date("m", strtotime($_SESSION['foire']->dateDebutFoire()))). " " . date("Y", strtotime($_SESSION['foire']->dateDebutFoire())); ?></h4>
             <h1><?php echo $user->id(); ?></h1>
             <h1><?php echo $user->name(); ?></h1>
             <h2><?php echo $user->fname(); ?></h2>
@@ -81,7 +112,6 @@ function printUser($userArr){
 }
 
 if (isset($_SESSION['foire'])) {
-
     if(empty($_POST)){
         $userMan = new UserManager();
         $userMan->loadUsersFromFoire($_SESSION['foire']->idFoire());
@@ -94,7 +124,6 @@ if (isset($_SESSION['foire'])) {
         }
         printUser($userArr);
     }
-
 }
 ?>
 </body>
